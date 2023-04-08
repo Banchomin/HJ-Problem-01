a527feb88ff47fc9d3452ff2faeaee32ec467201
banchomin <ban.chomin@gmail.com>
2023-04-08 14:21:46 +0900

    fin

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 const listModal = document.querySelector("#modal_1");
+const profileModal = document.querySelector("#modal_2");
 const list_btn = document.querySelector("#list");
-function show() {
+const profile_btn = document.querySelector("#profile");
+
+function showList() {
   if (listModal.style.display == "none") {
     listModal.style.display = "block";
+    profileModal.style.display = "none";
   } else {
     listModal.style.display = "none";
   }
 }
+function showProfile() {
+  if (profileModal.style.display == "none") {
+    profileModal.style.display = "block";
+    listModal.style.display = "none";
+  } else {
+    profileModal.style.display = "none";
+  }
+}
 
-list_btn.addEventListener("click", show);
+list_btn.addEventListener("click", showList);
+profile_btn.addEventListener("click", showProfile);
