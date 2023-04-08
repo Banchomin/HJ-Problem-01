143b22338baebdb7bcebfcdc893af4fc91f04924
banchomin <ban.chomin@gmail.com>
2023-04-08 23:09:55 +0900

    modal fast show 수정

--- js/app.js
+++ js/app.js
@@ -4,17 +4,20 @@ const list_btn = document.querySelector("#list");
 const profile_btn = document.querySelector("#profile");
 
 function showList() {
-  if (listModal.style.display == "none") {
+  if (listModal.style.display === "none") {
     listModal.style.display = "block";
     profileModal.style.display = "none";
+    console.log("hello");
   } else {
     listModal.style.display = "none";
   }
 }
+
 function showProfile() {
-  if (profileModal.style.display == "none") {
+  if (profileModal.style.display === "none") {
     profileModal.style.display = "block";
     listModal.style.display = "none";
+    console.log("bye");
   } else {
     profileModal.style.display = "none";
   }
