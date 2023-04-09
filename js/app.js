532537162d07652ea5ed898cfde81093c226da6e
banchomin <ban.chomin@gmail.com>
2023-04-09 20:05:38 +0900

    완성

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const listModal = document.querySelector("#modal_1");
 const profileModal = document.querySelector("#modal_2");
 const list_btn = document.querySelector("#list");
 const profile_btn = document.querySelector("#profile");
+const filterElem = document.querySelectorAll(":not(html, body, header, .header_menu, .header_menu *, #modal_1, #modal_1 *, #modal_2, #modal_2 *)");
 
 function showList() {
   if (listModal.style.display === "none") {
@@ -22,6 +23,17 @@ function showProfile() {
     profileModal.style.display = "none";
   }
 }
+
+function hideAll() {
+  profileModal.style.display = "none";
+  listModal.style.display = "none";
+}
+
+filterElem.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    hideAll();
+  });
+});
 
 list_btn.addEventListener("click", showList);
 profile_btn.addEventListener("click", showProfile);
